6327e2d135cc6c22f7a118d8467b04ac6fd558f2
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 12:59:30 +0900

    main

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ if st.sidebar.button('Custom'):
         st.sidebar.selectbox('육묘 끝 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
 
 st.slider('Select a Year', 1980, 2020)
+st.map()
