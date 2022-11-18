a5194ad97a31204cd63a8416310e8180bdba5e11
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 12:47:00 +0900

    main

--- main.py
+++ main.py
@@ -8,7 +8,4 @@ location_selectbox = st.sidebar.selectbox('시군구',('A','B','C'))
 cultiva_selectbox = st.sidebar.selectbox('작물',('사과','포도','가지','멜론','방울토마토','배추','브로콜리','상추','양배추','오이','참외','토마토','파프리카','호박','고구마','콩'))
 custom_button = st.sidebar.button('Custom')
 if st.sidebar.button('Custom'):
-    st.number_input('육묘 시작 월')
-    st.number_input('육묘 끝 월')
-    st.number_input('육묘 최저 기온')
-    st.number_input('육묘 최고 기온')
+    st.sidebar.number_input('')
