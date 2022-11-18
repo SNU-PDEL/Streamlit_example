1cb97dc4b5cf1e82d0e0903a9274298251569816
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 10:18:33 +0900

    commit message

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import streamlit as st
 st.text('hello Streamlit!')
+col1, col2 = st.columns([1,4])
 
-html = """
+with col1:
+    html = """
     <div style='
         background-color:red;
         color:white;
     '>
         안녕하세요
     </div>
-"""
-st.markdown(html, unsafe_allow_html = True)
+    """
 
-view = [100,150,30]
-st.write('# Youtube view')
-st.write('## raw')
-view
-st.write('## bar chart')
-st.bar_chart(view)
+with col2:
+    st.markdown(html, unsafe_allow_html = True)
 
-col1, col2 = st.columns([1,4])
+    view = [100,150,30]
+    st.write('# Youtube view')
+    st.write('## raw')
+    view
+    st.write('## bar chart')
+    st.bar_chart(view)
+
+
+
 
 
 
