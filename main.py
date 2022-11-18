27efe338812ca949e8c05420ef32363f44daa5a0
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 09:59:22 +0900

    streamlit main

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import streamlit as st
 st.text('hello Streamlit!')
+
+html = """
+    <div style='
+        background-color:red;
+        color:white;
+    '>
+        안녕하세요
+    </div>
+"""
+
+st.markdown(html, unsafe_allow_html = True)
