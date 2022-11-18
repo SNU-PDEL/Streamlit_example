ea8376f32eb5438dfdfbf21912041a0e1bcddd2c
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 13:57:03 +0900

    main

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ location_selectbox = st.sidebar.selectbox('시군구',('A','B','C'))
 cultiva_selectbox = st.sidebar.selectbox('작물',('사과','포도','가지','멜론','방울토마토','배추','브로콜리','상추','양배추','오이','참외','토마토','파프리카','호박','고구마','콩'))
 
 with st.form(key = 'columns_in_form'):
-    cols = st.columns(2)
+    col1_1, col1_2 = st.columns(2)
     if st.sidebar.button('Custom'):
-        for i, col in enumerate(cols):
-            st.sidebar.col.selectbox('육묘 시작 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
-            st.sidebar.col.selectbox('육묘 끝 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
-        submitted = st.form_submit_button('submit')
+        with col1_1:
+            st.sidebar.selectbox('육묘 시작 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
+        with col1_2:
+            st.sidebar.selectbox('육묘 끝 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
+    submitted = st.form_submit_button('submit')
 
 st.slider('Select a Year', 1980, 2020)
 col1, col2 = st.columns(2)
