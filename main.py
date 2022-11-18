3f42f39d3db91376725455495755d3a4cd8bf0e1
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 12:48:04 +0900

    main

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ import altair as alt
 st.title('Visualization homework')
 location_selectbox = st.sidebar.selectbox('시군구',('A','B','C'))
 cultiva_selectbox = st.sidebar.selectbox('작물',('사과','포도','가지','멜론','방울토마토','배추','브로콜리','상추','양배추','오이','참외','토마토','파프리카','호박','고구마','콩'))
-custom_button = st.sidebar.button('Custom')
 if st.sidebar.button('Custom'):
     st.sidebar.number_input('')
