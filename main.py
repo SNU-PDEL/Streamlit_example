cee515d6d75ac25329d830e3e3db9ddfbaf6fa1e
Wonjae <dustnch11@snu.ac.kr>
2022-11-18 13:52:23 +0900

    main

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ st.title('Visualization homework')
 location_selectbox = st.sidebar.selectbox('시군구',('A','B','C'))
 cultiva_selectbox = st.sidebar.selectbox('작물',('사과','포도','가지','멜론','방울토마토','배추','브로콜리','상추','양배추','오이','참외','토마토','파프리카','호박','고구마','콩'))
 
-if st.sidebar.button('Custom'):
-    st.sidebar.selectbox('육묘 시작 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
-    st.sidebar.selectbox('육묘 끝 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
+with st.form(key = 'columns_in_form'):
+    cols = st.beta_columns(2)
+    if st.sidebar.button('Custom'):
+        st.sidebar.col.selectbox('육묘 시작 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
+        st.sidebar.col.selectbox('육묘 끝 월',('1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'))
 
 st.slider('Select a Year', 1980, 2020)
 col1, col2 = st.columns(2)
 with col1:
     df2 = pd.DataFrame({'lat': [42.187,34.355], 'lon' : [123.71945,130.502]})
     st.map(df2)
+st.session_state.
