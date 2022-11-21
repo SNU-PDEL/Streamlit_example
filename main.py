d26e83061de87411df3880a6e661e2b0ac5aedf5
Wonjae <dustnch11@snu.ac.kr>
2022-11-21 13:37:45 +0900

    main

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ st.line_chart(df2)
 st.subheader('40년간 육묘적정온도 비교')
 df_1 = pd.read_csv((st3[st3['kEname']==location_selectbox]['number']+'.csv').values[0])
 df_2 = df_1.groupby(['Year','Mon']).mean()
-fruit = pd.read_csv('fruit.csv')
+fruit = pd.read_csv('fruit.csv', encoding = 'cp949')
 if int(fruit[fruit['작물명']== cultiva_selectbox]['육묘 끝']) - int(fruit[fruit['작물명']== cultiva_selectbox]['육묘 시작']) == 1:
     fruit2 = (df_2.xs(int(fruit[fruit['작물명']==cultiva_selectbox]['육묘 시작']),axis = 0, level = 1) + df_2.xs(int(fruit[fruit['작물명']==cultiva_selectbox]['육묘 끝']),axis = 0, level = 1))/2
 elif int(fruit[fruit['작물명']== cultiva_selectbox]['육묘 끝']) - int(fruit[fruit['작물명']== cultiva_selectbox]['육묘 시작']) == 2:
