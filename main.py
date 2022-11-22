fd259a2bf79770af0c9d2bd6390dcd8c8559f3b2
Wonjae <dustnch11@snu.ac.kr>
2022-11-22 19:24:32 +0900

    main

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ plt.plot(fruit2['optimal tmax'], color = 'lightgray')
 plt.plot(fruit2['tmax'], color = 'red')
 plt.plot(fruit2['tmin'], color = 'blue')
 
-plt.fill_between(x = fruit2.index, y1= fruit2['optimal tmin'],y2 =fruit2['tmin'], where = fruit2['tmin'] < fruit2['optimal tmin'],  facecolor = 'lightgray', alpha = 0.5)
+plt.fill_between(x = fruit2.index, y1= fruit2['optimal tmin'],y2 =fruit2['tmin'], where = (fruit2['tmin'] < fruit2['optimal tmin']),  facecolor = 'lightgray', alpha = 0.5)
 plt.ylim(bottom = 0)
 plt.legend(['optimal tmax','optimal tmin','tmax','tmin'])
 st.pyplot(fig)
