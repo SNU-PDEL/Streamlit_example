6b5fd29a99a88f90c17e1623cfea885506be5996
Wonjae <dustnch11@snu.ac.kr>
2022-11-22 12:38:45 +0900

    main

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ colors = ['lightgreen','white']
 plt.pie([data_frame['score'],data_frame['nonscore']],colors = colors, explode = (0.05,0.05))
 centre_circle = plt.Circle((0, 0), 0.90, fc='white')
 fig.gca().add_artist(centre_circle)
-plt.text(-1,0,'2018')
+plt.text(-0.2,0,'2018')
 st.pyplot(fig)
 
 
