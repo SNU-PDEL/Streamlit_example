38905bb3edf54c8947917c942012f6ae8e64c2f8
Wonjae <dustnch11@snu.ac.kr>
2022-11-22 10:20:45 +0900

    main

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ else:
 fruit2['optimal tmin'] = int(fruit[fruit['작물명']==cultiva_selectbox]['육묘 최저기온'])
 fruit2['optimal tmax'] = int(fruit[fruit['작물명']==cultiva_selectbox]['육묘 최고기온'])
 fig = plt.figure()
-plt.plot(fruit2[['tmax','tmin','optimal tmin','opitmal tmax']])
+plt.plot(fruit2[['tmax','tmin','optimal tmin','optimal tmax']])
 st.pyplot(fig)
 
 ####################################################################################
