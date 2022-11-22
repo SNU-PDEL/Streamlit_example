8e9669774d7378ff0a8267238cdb08da1eb08ffe
Wonjae <dustnch11@snu.ac.kr>
2022-11-22 14:39:06 +0900

    main

--- main.py
+++ main.py
@@ -67,7 +67,8 @@ with col1_2:
 
 labels = ['Oxygen','Hydrogen']
 values = [70, 30]
-fig = px.Figure(data=[px.Pie(labels=labels, values=values, hole=.9, color_discrete_map = {'Oxygen': 'lightgreen', 'Hydrogen' : 'white'})])
+fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.9)])
+fig.update_traces(marker = dict(colors = ['lightgreen','white']))
 st.plotly_chart(fig)
 
 
