07a1971dc94c786abfd223ac97eaecb1a3586495
Wonjae <dustnch11@snu.ac.kr>
2022-11-23 13:39:08 +0900

    main

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ st.sidebar.slider('년도',1980,2100)
 with st.form(key='Form2'):
     with st.sidebar:
         location_selectbox = st.selectbox('시군구',('강릉','강화','거제','거창','고흥','광주','구미','군산','금산','남원','남해','대관령','대구','대전','목포','문경','밀양','보령','보은','부산','부안','부여','산청','서귀포','서산','서울','성산','속초','수원','양평','여수','영덕','영주','영천','완도','울산','울진','원주','의성','이천','인제','인천','임실','장흥','전주','정읍','제주','제천','진주','천안','청주','추풍령','춘천','충주','통영','포항','합천','해남','홍천'))
-        cultiva_selectbox = st.selectbox('작물',('가지','고구마','멜론','배추','브로콜리','사과','상추','양배추','파프리카','호박','기타'))
+        cultiva_selectbox = st.selectbox('작물',('단감','당귀','배','복숭아','사과','인삼','천궁','포도','기타'))
         submitted2 = st.form_submit_button(label = 'submit')
 
 if st.sidebar.button('Custom'):
     with st.form(key = 'Form1'):
         with st.sidebar:  
-            st.slider('육묘 기간',1,12, (4,9))
-            st.slider('육묘 적정 기온', 10,40,(20,30))
+            st.slider('연 평균 기온', 10,40,(20,30))
             st.slider('생육 기간', 1,12, (4,9))
             st.slider('생육 적정 기온', 10,40,(20,30))
             submitted1 = st.form_submit_button(label = 'submit')
@@ -82,11 +81,19 @@ st.pyplot(fig)
 
 st.subheader(location_selectbox + '의 평균기온')
 st3 = pd.read_csv('st3.csv')
+fruit2 = pd.read_csv('fruit2.csv', encoding = 'cp949')
+fruit2_1 = fruit2[fruit2['작물명']== cultiva_selectbox]['연평균 최저기온']
+fruit2_2 = fruit2[fruit2['작물명']== cultiva_selectbox]['연평균 최고기온']
 df = pd.read_csv((st3[st3['kEname']==location_selectbox]['number']+'_new2.csv').values[0])
-df2 = (df.groupby('Year').mean()['tmax'] + df.groupby('Year').mean()['tmin'])/2
+df2 = df.groupby('Year').mean()['tmax'] 
+df3 = df.groupby('Year').mean()['tmin']
 fig = plt.figure(figsize=(11,4))
 plt.plot(df2, color = 'red')
-plt.legend(['tmax','tmin'])
+plt.plot(df3, color = 'blue')
+plt.plot(fruit2_1, color = 'lightgray')
+plt.plot(fruit2_2, color = 'lightgray')
+
+plt.legend(['tmax','tmin','optimal_tmin','optimal_tmax'])
 st.pyplot(fig)
 
 
