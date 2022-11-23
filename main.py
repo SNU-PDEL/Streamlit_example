d301af9a03b79623ef87d0be0f0efe6e5f2204e7
Wonjae <dustnch11@snu.ac.kr>
2022-11-23 18:32:57 +0900

    main

--- main.py
+++ main.py
@@ -144,13 +144,13 @@ st.subheader('')
 st.subheader(cultiva_selectbox + ' 생육시기에서 적정 생육 기온과 ' + location_selectbox + '의 기온 비교')
 st3 = pd.read_csv('st3.csv')
 fruit2 = pd.read_csv('fruit2.csv', encoding = 'cp949')
-fruit5_1 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 최저기온']
-fruit5_2 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 최고기온']
-fruit5_3 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 시작']
-fruit5_4 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 끝']
+fruit5_1 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 최저기온'][0]
+fruit5_2 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 최고기온'][0]
+fruit5_3 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 시작'][0]
+fruit5_4 = fruit2[fruit2['작물명']== cultiva_selectbox]['생육 끝'][0]
 df_13 = pd.read_csv((st3[st3['kEname']==location_selectbox]['number']+'.csv').values[0])
 df_130 = df_13[df_13['Year']==2012]
-if (fruit2[fruit2['작물명']== cultiva_selectbox]['생육 끝'][0]-fruit2[fruit2['작물명']== cultiva_selectbox]['생육 시작'][0]) == 2:
+if (fruit5_4-fruit5_3) == 2:
     df_131 = df_130[df_130['Mon']==fruit5_3[0]]
     df_132 = df_130[df_130['Mon']==fruit5_3[0]+1]
     df_231 = df_130[df_130['Mon']==fruit5_4[0]]
