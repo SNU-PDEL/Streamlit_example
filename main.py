858b552328e2517441c2daface061ddd553a2c68
Wonjae <dustnch11@snu.ac.kr>
2022-11-25 00:26:46 +0900

    main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pydeck as pdk
 
 
 st.title('Visualization Project')
-st.header('기후 시나리오에 따른 시군구별 작물 생육 적합도')
+st.subheader('기후 시나리오에 따른 시군구별 작물 생육 적합도')
 
 
 yearslider = st.sidebar.slider('년도',1981,2100)
