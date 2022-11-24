9241738d293c616328deb50cf891b0a04806e390
Wonjae <dustnch11@snu.ac.kr>
2022-11-24 19:39:42 +0900

    main

--- main.py
+++ main.py
@@ -16,19 +16,10 @@ with st.form(key='Form2'):
         location_selectbox = st.selectbox('시군구',('강릉','강화','거제','거창','고흥','광주','구미','군산','금산','남원','남해','대관령','대구','대전','목포','문경','밀양','보령','보은','부산','부안','부여','산청','서귀포','서산','서울','성산','속초','수원','양평','여수','영덕','영주','영천','완도','울산','울진','원주','의성','이천','인제','인천','임실','장흥','전주','정읍','제주','제천','진주','천안','청주','추풍령','춘천','충주','통영','포항','합천','해남','홍천'))
         cultiva_selectbox = st.selectbox('작물',('단감','당귀','배','복숭아','사과','인삼','천궁','포도','기타'))
         submitted2 = st.form_submit_button(label = 'submit')
-
-if st.sidebar.button('Custom'):
-    with st.form(key = 'Form1'):
-        with st.sidebar:  
-            st.slider('연 평균 기온', 10,40,(20,30))
-            st.slider('생육 기간', 1,12, (4,9))
-            st.slider('생육 적정 기온', 10,40,(20,30))
-            submitted1 = st.form_submit_button(label = 'submit')
-
-
+st3 = pd.read_csv('st3.csv')
 col1, col2 = st.columns([3,2])
 with col1:
-    df2 = pd.DataFrame({'lat': [42.187,34.355], 'lon' : [123.71945,130.502]})
+    df2 = st3[st3['kEname'] == '속초'][['Lon','Lat']]
     st.map(df2)
 with col2:
     image = Image.open(cultiva_selectbox + '.jpg')
