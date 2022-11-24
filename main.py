bd526f46d7eea0927e4db9cad6f31b37423d8553
Wonjae <dustnch11@snu.ac.kr>
2022-11-24 20:08:53 +0900

    main

--- main.py
+++ main.py
@@ -48,16 +48,6 @@ st.pydeck_chart(pdk.Deck(
     ),
     layers=[
         pdk.Layer(
-           'HexagonLayer',
-           data=st35,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
             'ScatterplotLayer',
             data=st35,
             get_position='[lon, lat]',
