04d106ee2bccf1f639dab03151b1db749d6af503
Wonjae <dustnch11@snu.ac.kr>
2022-11-25 00:13:52 +0900

    main

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import pydeck as pdk
 
 st.title('Visualization Project')
 st.header('Crop-suited location based on future climate data')
-st.subheder('서울대학교 농생명공학부 이노현')
-st.subheder('서울대학교 농생명공학부 정원재')
+st.subheader('서울대학교 농생명공학부 이노현')
+st.subheader('서울대학교 농생명공학부 정원재')
 yearslider = st.sidebar.slider('년도',1981,2100)
 with st.form(key='Form2'):
     with st.sidebar:
