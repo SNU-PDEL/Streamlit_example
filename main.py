ab11beb1bac004fbcfd1bc9e4d3a9304b3f0887a
Wonjae <dustnch11@snu.ac.kr>
2022-11-24 18:59:43 +0900

    main

--- main.py
+++ main.py
@@ -311,8 +311,8 @@ heatmap.index = heatmap['Year']
 del heatmap['Year']
 fig, ax = plt.subplots(figsize=(24, 20))
 im = ax.matshow(heatmap, cmap='Reds')
-ax.set_xticks(np.arange(len(heatmap.columns)), labels=heatmap.columns, size = 25)
-ax.set_yticks(np.arange(len(heatmap.index)), labels=heatmap.index, size = 25)
+ax.set_xticks(np.arange(len(heatmap.columns)), labels=heatmap.columns, size = 15)
+ax.set_yticks(np.arange(len(heatmap.index)), labels=heatmap.index, size = 10)
 ax.grid(False)
 fig.colorbar(im)
 st.pyplot(fig)
