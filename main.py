1650e14b007b3ee30b97402f1348f0f7e0c514ca
Wonjae <dustnch11@snu.ac.kr>
2022-11-24 18:59:04 +0900

    main

--- main.py
+++ main.py
@@ -307,13 +307,15 @@ st.pyplot(fig)
 st.subheader('')
 st.subheader(cultiva_selectbox + '의 육묘 적정 기온구간과 ' + location_selectbox+'의 기온구간의 차이')   
 heatmap = pd.read_csv('속초_단감.csv')
-
+heatmap.index = heatmap['Year']
+del heatmap['Year']
 fig, ax = plt.subplots(figsize=(24, 20))
 im = ax.matshow(heatmap, cmap='Reds')
+ax.set_xticks(np.arange(len(heatmap.columns)), labels=heatmap.columns, size = 25)
+ax.set_yticks(np.arange(len(heatmap.index)), labels=heatmap.index, size = 25)
 ax.grid(False)
 fig.colorbar(im)
 st.pyplot(fig)
 
-#ax.set_xticks(np.arange(len(da58.keys())), labels=da58.keys(), size = 25)
-#ax.set_yticks(np.arange(len(year)), labels=year, size = 25)
+
 
